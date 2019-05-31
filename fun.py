15cb18d1a1ab0877b9e2692ad570004127a2be1c
critter-johnson-bi <43154693+critter-johnson-bi@users.noreply.github.com>
2019-05-31 17:22:09 -0400

    fixed shelve errors

--- fun.py
+++ fun.py
@@ -1,10 +1,13 @@
 # get some fun functions
-import funfuncs
+from funfuncs import *
 
 start()
 # --- your code below ---
 
-
+set_as_output(21, "dog")
+turn_on()
+wait(5)
+turn_off()
 
 # --- *************** ---
 end()
--- funfuncs.py
+++ funfuncs.py
@@ -9,55 +9,62 @@ import shelve
 def start():
 	GPIO.setmode(GPIO.BCM)
 	# creates/clears the shelf object
-	with shelve.open("pins") as pins:
-		pins["in"] = {}
-		pins["out"] = {}
+	pins = shelve.open("pins")
+	pins["in"] = {}
+	pins["out"] = {}
+	pins.close()
 
 # cleans up and turns off everything that isn't off
 def end():
 	turn_off()
 	GPIO.cleanup()
 
+# waits a specified amount of time
+def wait(sec):
+	time.sleep(sec)
+
 # sets up a pin for output and adds it to the shelf
 def set_as_output(pin, name=None):
-	with shelve.open("pins") as pins:
-		pins["out"][pin] = pin
-		if name is not None:
-			pins["out"][name] = pin
+	pins = shelve.open("pins")
+	pins["out"][pin] = pin
+	if name is not None:
+		pins["out"][name] = pin
+	pins.close()
 	GPIO.setup(pin, GPIO.OUT)
 
 # sets a pin as input and adds it to the shelf
 def set_as_input(pin, name=None):
-	with shelve.open("pins") as pins:
+	pins = shelve.open("pins")
+	pins["in"][name] = pin
+	if name is not None:
 		pins["in"][name] = pin
-		if name is not None:
-			pins["in"][name] = pin
+	pins.close()
 	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # turns on a pin or pins by pin number or by name,
 # or pass nothing to turn on all lights
 def turn_on(*args):
+	pins = shelve.open("pins")
 	# turns on all lights if no arguments passed
 	if len(args) == 0:
-		with shelve.open("pins") as pins:
-			for name, pin in pins["out"].items():
-				GPIO.output(pin, True)
+		for name, pin in pins["out"].items():
+			GPIO.output(pin, True)
 	# get the pin numbers from the args
 	else:
-		with shelve.open("pins") as pins:
-			for name in args:
-				GPIO.output(pins[name], True)
+		for name in args:
+			GPIO.output(pins[name], True)
+	pins.close()
 
 # turns off a pin or pins by pin number or name,
 # or pass nothing to turn off all lights
 def turn_off(*args):
+	pins = shelve.open("pins")
 	# turns on all lights if no arguments passed
 	if len(args) == 0:
-		with shelve.open("pins") as pins:
-			for name, pin in pins["out"].items():
-				GPIO.output(pin, False)
+		for name, pin in pins["out"].items():
+			GPIO.output(pin, False)
 	# get the pin numbers from the args
 	else:
-		with shelve.open("pins") as pins:
-			for name in args:
-				GPIO.output(pins[name], False)
+		for name in args:
+			GPIO.output(pins[name], False)
+	pins = shelve.open("pins")
